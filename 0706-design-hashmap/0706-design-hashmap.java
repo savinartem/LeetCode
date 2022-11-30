eff60b18d2001fc5e55bead862357a02a46f08a2
savinartem <74740204+savinartem@users.noreply.github.com>
2022-11-30 05:32:38 -0500

    Time: 119 ms (18.15%), Space: 110.4 MB (16.10%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -0,0 +1,29 @@
+class MyHashMap {
+    int[] set=new int[1000001]; 
+
+    public MyHashMap() {
+        for(int i=0; i<1000001;i++){
+            set[i]=-1;
+        }
+    }
+    
+    public void put(int key, int value) {
+        set[key]=value;
+    }
+    
+    public int get(int key) {
+        return set[key];
+    }
+    
+    public void remove(int key) {
+        set[key]=-1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
