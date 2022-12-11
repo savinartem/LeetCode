3ffaeb253baa261640dc49397d43c7d4e98a3c50
savinartem <74740204+savinartem@users.noreply.github.com>
2022-12-11 17:38:19 -0500

    Time: 0 ms (100.00%), Space: 40.8 MB (44.99%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int tribonacci(int n) {
+      int[] nums=new int[38];
+      nums[0]=0;
+      nums[1]=1;
+      nums[2]=1;
+
+      for(int i=3;i<=n;i++){
+          nums[i]=nums[i-1]+nums[i-2]+nums[i-3];
+      }
+      return nums[n];
+    }
+}
