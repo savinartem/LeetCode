0fc88e01fb1c1e0bceca261734fecfe234ff9a2a
savinartem <74740204+savinartem@users.noreply.github.com>
2022-12-19 00:26:45 -0500

    Time: 0 ms (100.00%), Space: 42.3 MB (6.06%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,18 +10,15 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        //base case
-        if(head.next==null){
-            return head;
-        }
-        //define slow and fast ptr's
+     if(head.next==null){return head; }
         ListNode slow=head;
-        ListNode fast=head;
+        ListNode fast= head;
         
         while(fast!=null && fast.next!=null){
             fast=fast.next.next;
             slow=slow.next;
         }
+        
         return slow;
     }
 }
