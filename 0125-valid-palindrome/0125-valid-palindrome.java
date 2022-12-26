fc4b3b8489349a16e30442a731125a86a7432313
savinartem <74740204+savinartem@users.noreply.github.com>
2022-12-26 09:06:57 -0500

    Time: 3 ms (98.88%), Space: 42.1 MB (97.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase();
+       s=s.toLowerCase();
         int left=0;
         int right=s.length()-1;
         
         while(left<right){
-            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
-                left++;
-            }
-            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
-                right--;
-            }
-            if(left<right && s.charAt(left)!=s.charAt(right)){
-                return false;
-            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){ left++; }
+            
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))) { right--; }
+            
+            if(left<right && s.charAt(left)!=s.charAt(right)){ return false; }
+            
             left++;
             right--;
         }
         return true;
     }
 }
+
+/*
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+*/
