32adb4d26288db3db2fb238fbdabb5a83caa4958
savinartem <74740204+savinartem@users.noreply.github.com>
2022-12-27 09:17:24 -0500

    Time: 1 ms (99.64%), Space: 40.3 MB (83.48%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack= new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
         
-        for (int i=0; i<s.length();i++){
+        for(int i=0; i<s.length(); i++){
             char ch=s.charAt(i);
-            if(ch=='[' || ch=='{' || ch=='('){
-                stack.push(ch);
-            }else if(ch==']'){
-                if(stack.isEmpty() || stack.pop()!='['){
-                    return false;
-                }
-            }else if(ch==')'){
-                if(stack.isEmpty() || stack.pop()!= '('){
-                    return false;
-                }
-            }else if (ch=='}'){
-                if(stack.isEmpty() || stack.pop()!='{'){
-                    return false;
-                }
+            
+            if(ch=='[' || ch=='{' || ch=='('){ stack.push(ch); }
+            else if(ch==']'){
+                if(stack.isEmpty() || stack.pop()!='[') { return false; }
+            }
+            else if(ch=='}'){
+                if(stack.isEmpty() || stack.pop()!='{') { return false; }
+            }
+            else if(ch==')'){
+                if(stack.isEmpty() || stack.pop()!='(') { return false; }
             }
         }
         return stack.isEmpty();
