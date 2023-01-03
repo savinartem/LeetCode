563b9bafba32f0a45ae79f24d3d99ae41f2f834a
savinartem <74740204+savinartem@users.noreply.github.com>
2023-01-03 11:21:16 -0500

    Time: 2 ms (56.41%), Space: 42.4 MB (47.72%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean digitCount(String num) {
-        Map<Integer, Integer> map= new HashMap<>();
-        
-        for(char ch: num.toCharArray()){ map.put(ch-'0', map.getOrDefault(ch-'0',0)+1); }
-        
-        for(int i=0; i<num.length();i++){
-            if(map.containsKey(i)){
-                int temp=map.get(i);
-                if(num.charAt(i)-'0'!=temp){ return false; }
-            }
-            else if(num.charAt(i)-'0'!=0){ return false; }
+        int[] temp = new int[10];
+        for(char c:num.toCharArray()){
+            temp[c-'0']++;
         }
+        for(int i=0;i<num.length();i++){
+            int digit=num.charAt(i)-'0';
+            if(digit!=temp[i]){
+                return false;
+            }
+        }
+        
         return true;
     }
 }
