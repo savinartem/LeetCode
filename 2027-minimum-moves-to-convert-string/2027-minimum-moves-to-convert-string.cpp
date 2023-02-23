30b794998904a954c21a6d88fadae9b2fd141e98
savinartem <74740204+savinartem@users.noreply.github.com>
2023-02-23 05:02:31 -0500

    Time: 4 ms (36.80%), Space: 6.4 MB (88.14%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+      int count=0;
+        
+        for(int i=0; i<s.size(); )
+        {
+            if(s[i]=='X')
+            {
+                count++;
+                i+=3;
+            }
+            else
+            {
+                i++;
+           }
+        }
+        return count;
+    }
+};
