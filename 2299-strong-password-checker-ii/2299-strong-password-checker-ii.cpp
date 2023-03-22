ce8069274cc1092f80533f4ac096f73ca636d114
savinartem <74740204+savinartem@users.noreply.github.com>
2023-03-22 04:58:24 -0400

    Time: 4 ms (36.75%), Space: 6.1 MB (58.39%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool strongPasswordCheckerII(string password) {
+        int low=0;
+        int up=0;
+        int digit=0;
+        int special=0;
+        
+        if(password.length()<8){ return false; }
+        
+        for(int i=0; i<password.length(); i++)
+        {
+            if(isupper(password[i])){ up++; }
+            
+            else if(islower(password[i])) { low++;}
+            
+            else if(isdigit(password[i])) { digit++; }
+            
+            else { special++; }
+                
+            if(i<password.length()-1 && password[i]==password[i+1]) { return false; }
+        }
+        
+        if (low>0 && up>0 && digit>0 && special>0){  return true;}
+        return false;
+        
+    }
+};
