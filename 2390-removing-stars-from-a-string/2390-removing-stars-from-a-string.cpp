b9431e3d166122a30a78e12f74bfb0aa0c269a0f
savinartem <74740204+savinartem@users.noreply.github.com>
2023-04-11 04:27:05 -0400

    Time: 79 ms (82.87%), Space: 25.9 MB (72.25%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string str="";
+        
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]=='*')
+            {
+                str.pop_back();
+            }
+            else
+            {
+                str+=s[i];
+            }
+        }
+        return str;
+    }
+};
