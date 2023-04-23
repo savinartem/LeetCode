fa1166a5c878917a592f022467b0adf79e404a20
savinartem <74740204+savinartem@users.noreply.github.com>
2023-04-23 04:51:01 -0400

    Time: 5 ms (42.86%), Space: 6 MB (14.29%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+                if(arrivalTime+delayedTime==24) { return 0; }
+        else if(arrivalTime+delayedTime>24){ return (arrivalTime+delayedTime)-24; }
+        else return arrivalTime+delayedTime;
+    }
+};
