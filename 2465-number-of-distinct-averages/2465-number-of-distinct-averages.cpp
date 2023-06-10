243ab3d8ff6c541ca586c870272083ee1705fe14
savinartem <74740204+savinartem@users.noreply.github.com>
2023-06-10 06:53:23 -0400

    Time: 0 ms (100.00%), Space: 7.1 MB (56.60%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int distinctAverages(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+         sort(nums.begin(), nums.end());
         int i=0;
         int j=nums.size()-1;
         set<int> s;
